0ca91e339f98ae30586b746302f9bf46dcc445ea
AnKiWoong <ankiwoong@gmail.com>
2020-06-01 21:15:10 +0900

    장고 어드민 주요 기능 살표보고 적용하기 2

--- fastadmin/admin.py
+++ fastadmin/admin.py
@@ -8,6 +8,8 @@ class DisplayEmployee(admin.ModelAdmin):
     list_display = ('emp_no', 'first_name', 'last_name',
                     'birth_date', 'gender', 'hire_date')
     search_fields = ['first_name']
+    list_filter = ('gender',)
+    ordering = ('last_name', 'hire_date')
 
 
 admin.site.register(Employees, DisplayEmployee)
