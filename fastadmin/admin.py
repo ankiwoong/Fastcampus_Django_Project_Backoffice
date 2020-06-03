4c3c59853442d663cdff422d1952a4b545633765
AnKiWoong <ankiwoong@gmail.com>
2020-06-03 12:23:51 +0900

    Django admin에 이미지 출력

--- fastadmin/admin.py
+++ fastadmin/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from rangefilter.filter import DateRangeFilter, DateTimeRangeFilter
 
 from .models import Employees, Departments, Cat
@@ -17,21 +18,20 @@ class DisplayEmployee(admin.ModelAdmin):
 class DisplayCat(admin.ModelAdmin):
     fields = ('name', 'age', 'photo')
     list_display = ('name', 'age', 'photo', 'get_image')
-    readonly_fields = ['name']
 
     def get_image(self, obj):
         return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
             url=obj.photo.url,
             width=200,
-            height=200,
+            height=50,
         )
         )
 
-    def has_add_permission(self, request):
-        return False
+    # def has_add_permission(self, request):
+    #     return False
 
-    def has_delete_permission(self, request, obj=None):
-        return False
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
 
 
 admin.site.register(Employees, DisplayEmployee)
